cd882737144187bc34b8ed5c400cbec16ffea44b
salma basem <20191743@fue.edu.eg>
2022-05-21 20:52:54 +0200

    o

--- Regestration/personSignUP.aspx.cs
+++ Regestration/personSignUP.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Regestration
 {
@@ -16,6 +17,17 @@ namespace Regestration
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlConnection conn = new SqlConnection();
+            //conn.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|database1.mdf" ;
+            conn.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Database1.mdf;Integrated Security=True";
+            // create insert statement
+            string strINSERT = String.Format("INSERT INTO member values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')", txt_fn.Text, txt_ln.Text, RRL.SelectedValue,txt_DOB.Text, txt_e.Text,txt_address.Text, txt_phone.Text, DDP.SelectedValue, txt_UN.Text, txt_PASS.Text,DDP_role.SelectedValue);
+            // create sql command
+            SqlCommand cmd = new SqlCommand(strINSERT, conn);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
 
         }
     }
